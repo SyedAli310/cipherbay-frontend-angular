2b7f7a07026c88d5fa91188ea202f27feb1b72c0
SyedAli310 <alizaidi310@gmail.com>
2022-03-20 20:28:14 +0530

    closing sidebar on route change

--- src/app/components/side-navbar/side-navbar.component.ts
+++ src/app/components/side-navbar/side-navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Router, NavigationStart } from '@angular/router';
 
 @Component({
   selector: 'app-side-navbar',
@@ -8,12 +9,21 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 export class SideNavbarComponent implements OnInit {
   @Input() isSidebarOpen!: boolean;
   @Output() closeSidebarEvent = new EventEmitter();
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
-    // this.isSidebarOpen = true;
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        this.routerChangeMethod(event.url);
+      }
+    });
   }
   closeSidebar() {
     this.closeSidebarEvent.emit();
+  }
+
+  routerChangeMethod(url: any) {
+    console.log('route changed to: ' + url);
+    this.closeSidebar();
   }
 }
