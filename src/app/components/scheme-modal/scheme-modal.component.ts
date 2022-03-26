3219b66da55857d9de7c0dffb43519194ceec351
SyedAli310 <alizaidi310@gmail.com>
2022-03-27 02:53:06 +0530

    implemented sheme search in modal

--- src/app/components/scheme-modal/scheme-modal.component.ts
+++ src/app/components/scheme-modal/scheme-modal.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
-import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faTimes, faSearch } from '@fortawesome/free-solid-svg-icons';
 import { SchemeView } from 'src/app/SchemeView';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { CipherbayService } from 'src/app/services/cipherbay.service';
 
 @Component({
@@ -11,12 +12,16 @@ import { CipherbayService } from 'src/app/services/cipherbay.service';
 export class SchemeModalComponent implements OnInit {
   //icons
   faTimes = faTimes;
+  faSearch = faSearch;
 
   @Input() selectedScheme!: SchemeView;
   @Input() isOpen?: boolean;
   @Input() schemes!: SchemeView[];
   @Output() closeModalEvent = new EventEmitter();
   @Output() selectSchemeEvent = new EventEmitter();
+
+  isSearchSchemeFormVisible: boolean = false;
+
   constructor(private cipherBayService: CipherbayService) {}
 
   ngOnInit(): void {}
@@ -40,5 +45,26 @@ export class SchemeModalComponent implements OnInit {
         failure.innerHTML = 'failed to load schemes -> ' + error.statusText;
       }
     );
+  }
+
+  searchSchemeForm = new FormGroup({
+    search: new FormControl(''),
+  });
+
+  searchScheme() {
+    const searchQuery = this.searchSchemeForm.get('search')?.value;
+    const schemeList = document.querySelectorAll('.scheme-select-btn');
+    schemeList.forEach((scheme: any) => {
+      const schemeName = scheme.innerHTML;
+      if (schemeName.includes(searchQuery)) {
+        scheme.style.display = 'block';
+      } else {
+        scheme.style.display = 'none';
+      }
+    });
+  }
+
+  toggleSearchForm() {
+    this.isSearchSchemeFormVisible = !this.isSearchSchemeFormVisible;
   }
 }
