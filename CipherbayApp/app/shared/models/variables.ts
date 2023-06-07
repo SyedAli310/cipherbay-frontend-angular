5c4443730c4ca05910f768b5e9936bc3cbc48cc7
Syed Ali <alizaidi310@gmail.com>
2023-06-08 00:09:50 +0530

    changed bse url to vercel prod

--- CipherbayApp/app/shared/models/variables.ts
+++ CipherbayApp/app/shared/models/variables.ts
@@ -1,4 +1,4 @@
-// export const BASE_URL = 'https://cipherbay-api.vercel.app';
-export const BASE_URL = 'http://localhost:5000';
+export const BASE_URL = 'https://cipherbay-api.vercel.app';
+// export const BASE_URL = 'http://localhost:5000';
 export const BASE_API_URL = `${BASE_URL}/api/v1`;
 export const IS_REGISTERATION_ENABLED = true;
